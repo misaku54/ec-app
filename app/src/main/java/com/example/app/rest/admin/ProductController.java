0b3b7958e07123aaba690795a637473e12c7dc1d
EISEI TEZUKA <eisei0504@outlook.jp>
2025-09-29 13:02:00 +0900

    fix:フォルダ名の変更

--- app/src/main/java/com/example/app/rest/admin/ProductController.java
+++ app/src/main/java/com/example/app/rest/admin/ProductController.java
@@ -1,4 +1,4 @@
-package com.example.app.controller.admin;
+package com.example.app.rest.admin;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
