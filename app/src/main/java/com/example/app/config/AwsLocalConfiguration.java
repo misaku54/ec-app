b6b7a12057676bc262e3a9d45f42d67482e47837
EISEI TEZUKA <eisei0504@outlook.jp>
2025-10-12 19:58:46 +0900

    s3clientのBeanを登録用メソッド作成

--- app/src/main/java/com/example/app/config/AwsLocalConfiguration.java
+++ app/src/main/java/com/example/app/config/AwsLocalConfiguration.java
@@ -1,7 +1,37 @@
 package com.example.app.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
 
 @Configuration
 public class AwsLocalConfiguration {
+
+  @Value("${cloud.aws.s3.endpoint}")
+  private String endpoint;
+
+  @Value("${cloud.aws.s3.region}")
+  private String region;
+
+  @Value("${cloud.aws.credentials.access-key}")
+  private String accessKey;
+
+  @Value("${cloud.aws.credentials.secret-key}")
+  private String secretKey;
+
+  @Bean
+  public S3Client s3ClientCreate() {
+    return S3Client.builder()
+      .endpointOverride(URI.create(endpoint))
+      .region(Region.of(region))
+      .credentialsProvider(StaticCredentialsProvider.create(
+        AwsBasicCredentials.create(accessKey, secretKey)
+      )).build();
+  }
 }
