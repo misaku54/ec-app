60bcf0b686d896110f9fd327cd0845e2b5e64ab8
EISEI TEZUKA <eisei0504@outlook.jp>
2025-11-18 13:57:50 +0900

    fe練習

--- frontend/src/practice/Page1.tsx
+++ frontend/src/practice/Page1.tsx
@@ -1,7 +1,13 @@
+import { Link, Outlet } from "react-router";
 export const Page1 = () => {
   return (
     <div>
       <h1>Page1</h1>
+      <Link to="detailA">DetailA</Link>
+      <br />
+      <Link to="detailB">DetailB</Link>
+      <br />
+      <Outlet />
     </div>
   );
 }
--- frontend/src/practice/RoutePractice.tsx
+++ frontend/src/practice/RoutePractice.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Link, Routes, Route } from "react-router";
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
 import { Page2 } from "./Page2";
+import { Page1DetailA } from "./Page1DetailA";
+import { Page1DetailB } from "./Page1DetailB";
 
 export const RoutePractice = () => {
   return (
@@ -12,13 +14,13 @@ export const RoutePractice = () => {
         <Link to="/page1">Page1</Link>
         <br />
         <Link to="/page2">Page2</Link>
-        <Home />
-        <Page1 />
-        <Page2 />
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page1" element={<Page1 />}>
+          <Route path="detailA" element={<Page1DetailA />} />
+          <Route path="detailB" element={<Page1DetailB />} />
+        </Route>
         <Route path="/page2" element={<Page2 />} />
       </Routes>
     </BrowserRouter>
