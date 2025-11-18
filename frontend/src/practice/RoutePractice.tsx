b0d34ed0a0ad37db0a39f68d4a08009cd5145d9e
EISEI TEZUKA <eisei0504@outlook.jp>
2025-11-18 14:07:41 +0900

    fe練習

--- frontend/src/practice/RoutePractice.tsx
+++ frontend/src/practice/RoutePractice.tsx
@@ -1,9 +1,5 @@
-import { BrowserRouter, Link, Routes, Route } from "react-router";
-import { Home } from "./Home";
-import { Page1 } from "./Page1";
-import { Page2 } from "./Page2";
-import { Page1DetailA } from "./Page1DetailA";
-import { Page1DetailB } from "./Page1DetailB";
+import { BrowserRouter, Link } from "react-router";
+import { Router } from "./router/Router";
 
 export const RoutePractice = () => {
   return (
@@ -15,14 +11,7 @@ export const RoutePractice = () => {
         <br />
         <Link to="/page2">Page2</Link>
       </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/page1" element={<Page1 />}>
-          <Route path="detailA" element={<Page1DetailA />} />
-          <Route path="detailB" element={<Page1DetailB />} />
-        </Route>
-        <Route path="/page2" element={<Page2 />} />
-      </Routes>
+      <Router />
     </BrowserRouter>
   );
 }
