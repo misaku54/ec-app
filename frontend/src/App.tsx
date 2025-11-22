cb16bb1853feae5a5588cb4535bd377e3d1e81a5
EISEI TEZUKA <eisei0504@outlook.jp>
2025-11-22 17:00:55 +0900

    fe練習

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,19 @@ import './App.css'
 import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { SearchInput } from './components/atoms/molecules/SearchInput';
+import { UserCard } from './components/atoms/organisms/user/UserCard';
+import "./practice/styles.css";
+
+const user = {
+  name: "name1",
+  image: "https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13",
+  email: "12345@example.com",
+  phone: "090-1111-2222",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
 
 export const App = () => {
   return (
@@ -9,6 +22,7 @@ export const App = () => {
       <PrimaryButton>ボタン</PrimaryButton>
       <SecondaryButton>セカンド</SecondaryButton>
       <SearchInput />
+      <UserCard user={user}/>
     </div>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
-import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 
 // import './index.css'
 // import App from './App.tsx'
