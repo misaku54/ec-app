fb3bf9d4834476f05bbdb999a905c455ab7f45d2
EISEI TEZUKA <eisei0504@outlook.jp>
2025-11-30 19:47:08 +0900

    chakra ui導入

--- frontend/src/Call.tsx
+++ frontend/src/Call.tsx
@@ -2,7 +2,13 @@ import axios from "axios";
 import { useState } from "react"; 
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
 
+const user: User = {
+  name: "eee",
+  // hobbies: ["絵", "映画"]
+}
 
 export const Call = () => {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -17,6 +23,7 @@ export const Call = () => {
   }
   return (
     <div className="App">
+      <UserProfile user={user}/>
       <button onClick={onClickFetchDate}>データ取得</button>
       {todos.map((todo) => (
         <Todo key={todo.id} title={todo.title} userId={todo.userId} completed={todo.completed}/>
