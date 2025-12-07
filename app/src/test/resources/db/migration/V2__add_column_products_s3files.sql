b08d4efabc934ff1e15614a3b9194020ad99f83f
sei <listen_spy_air_159493@i.softbank.jp>
2025-12-07 16:36:14 +0900

    fix:クエリに;をつける

--- app/src/test/resources/db/migration/V2__add_column_products_s3files.sql
+++ app/src/test/resources/db/migration/V2__add_column_products_s3files.sql
@@ -1,2 +1,2 @@
-alter table products add column del_flg boolean not null default false
-alter table s3_files add column del_flg boolean not null default false
+alter table products add column del_flg boolean not null default false;
+alter table s3_files add column del_flg boolean not null default false;
