101f16b35bf6cc7fac5c68b8b375d6fa1528b553
sei <listen_spy_air_159493@i.softbank.jp>
2025-12-09 20:27:57 +0900

    fe練習

--- frontend/src/components/pages/Login.tsx
+++ frontend/src/components/pages/Login.tsx
@@ -1,5 +1,20 @@
 import { FC, memo } from "react";
+import { Box, Flex, Heading, Input, Stack,Button } from "@chakra-ui/react";
+import { Separator } from "@chakra-ui/react";
 
 export const Login: FC = memo(() => {
-  return <p>ログインページです</p>;
+  return (
+    <Flex align="center" justify="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" boxShadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザー管理アプリ
+        </Heading>
+        <Separator my={4} />
+        <Stack gap={6} py={4} px={10}>
+          <Input placeholder="ユーザーID" />
+          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8}}>ログイン</Button>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 })
