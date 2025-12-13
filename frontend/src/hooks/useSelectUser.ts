e20921a2316936045510335ce1b190ff4f372595
sei <listen_spy_air_159493@i.softbank.jp>
2025-12-13 21:54:47 +0900

    fe練習

--- frontend/src/hooks/useSelectUser.ts
+++ frontend/src/hooks/useSelectUser.ts
@@ -1,5 +1,25 @@
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { User } from "../types/api/user";
+
+type Props = {
+  id: number;
+  users: Array<User>;
+}
 
 // ユーザーの詳細情報を取得するhooks
 export const useSelectUser = () => {
+  const [selectedUser, setSelectedUser] = useState<User>();
 
+  const onSelectUser = useCallback((props: Props) => {
+    const { id, users } = props;
+
+    axios
+      .get<User>("https://jsonplaceholder.typicode.com/users/1")
+      .then((res) => setSelectedUser(res.data))
+      .catch(() => alert('ユーザーを取得できませんでした。'))
+      .finally(() => )
+
+
+  },[])
 }
