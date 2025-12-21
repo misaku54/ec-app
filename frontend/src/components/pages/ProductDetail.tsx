3590ab09815883693e8daa6fe0010d3bd6b353e4
sei <listen_spy_air_159493@i.softbank.jp>
2025-12-21 22:41:33 +0900

    FE商品詳細の実装2

--- frontend/src/components/pages/ProductDetail.tsx
+++ frontend/src/components/pages/ProductDetail.tsx
@@ -1,11 +1,22 @@
-export const ProductDetail = () => {
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import { useSelectProduct } from "../../hooks/useSelectProduct";
+
+export const ProductDetail:React.FC = () => {
+  const { loading, selectedProduct ,selectProduct } = useSelectProduct();
+  const { id = '0' } = useParams();
+  useEffect(() => selectProduct(id), []);
+  
   return (
     <>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div>{selectedProduct?.id}</div>
+      <div>{selectedProduct?.name}</div>
+      <div>{selectedProduct?.description}</div>
+      <div>{selectedProduct?.price}</div>
+      <div>{selectedProduct?.stock}</div>
+      <div>{selectedProduct?.createdAt}</div>
+      <div>{selectedProduct?.updatedAt}</div>
+      <img src={selectedProduct?.imageUrl ?? null} width={200} height={200}/>
     </>
   )
 }
