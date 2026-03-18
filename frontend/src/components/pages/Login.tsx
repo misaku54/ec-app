a74913e21515cef57e650a08bba54c927097ec9a
sei <listen_spy_air_159493@i.softbank.jp>
2026-03-18 22:29:07 +0900

    ログインフォームの実装

---
+++ frontend/src/components/pages/Login.tsx
@@ -0,0 +1,89 @@
+import { ErrorMessage } from "@hookform/error-message";
+import { useForm, type SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
+
+export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+    reset();
+  };
+
+  return (
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+          Sign in
+        </h2>
+      </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                {...register("email", {
+                  required: "メールアドレスは必須です",
+                  maxLength: 60,
+                })}
+                type="email"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              />
+              <p className="mt-1 text-sm text-red-600">
+                <ErrorMessage errors={errors} name="email" />
+              </p>
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm/6 font-medium text-gray-900"
+            >
+              Password
+            </label>
+            <div className="mt-2">
+              <input
+                {...register("password", {
+                  required: "パスワードは必須です",
+                  minLength: {
+                    value: 8,
+                    message: "パスワードは8文字以上でなくてはなりません",
+                  },
+                })}
+                type="password"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              />
+              <p className="mt-1 text-sm text-red-600">
+                <ErrorMessage errors={errors} name="password" />
+              </p>
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,16 +1,17 @@
 import { Route, Routes } from "react-router";
+import { Login } from "../components/pages/Login";
 import { ProductDetail } from "../components/pages/ProductDetail";
 import { ProductManegement } from "../components/pages/ProductManegement";
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<div className="bg-red-200">ログイン画面</div>}/>
+      <Route path="/" element={<Login />} />
       <Route path="/admin/product">
-        <Route path="list" element={<ProductManegement />}/>
-        <Route path=":id" element={<ProductDetail/>}/>
+        <Route path="list" element={<ProductManegement />} />
+        <Route path=":id" element={<ProductDetail />} />
       </Route>
-      <Route path="*" element={<div>404</div>}/>
+      <Route path="*" element={<div>404</div>} />
     </Routes>
   );
-}
+};
