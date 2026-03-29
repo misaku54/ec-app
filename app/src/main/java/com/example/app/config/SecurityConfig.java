df9c9c0ca897577494c16f678fd02a8b0e520b11
sei <listen_spy_air_159493@i.softbank.jp>
2026-03-29 22:08:29 +0900

    cors設定

--- app/src/main/java/com/example/app/config/SecurityConfig.java
+++ app/src/main/java/com/example/app/config/SecurityConfig.java
@@ -14,6 +14,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @RequiredArgsConstructor
@@ -29,6 +33,18 @@ public class SecurityConfig {
   @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
     return config.getAuthenticationManager();
+  }
+
+  @Bean
+  UrlBasedCorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3003"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
+    configuration.setAllowCredentials(true);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
   @Bean
@@ -62,6 +78,7 @@ public class SecurityConfig {
     // authenticationEntryPointは未認証（ログインしていない）ユーザーがアクセスしたとき 401
     // accessDeniedHandlerは認証済みだが権限が足りないユーザーがアクセスしたとき　403
     http
+      .cors(cors -> cors.configurationSource(corsConfigurationSource()))
       .csrf(AbstractHttpConfigurer::disable)
       .exceptionHandling(ex -> ex
         .authenticationEntryPoint((req, res, auth) -> {
