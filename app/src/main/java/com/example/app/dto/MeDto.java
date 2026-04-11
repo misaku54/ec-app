7c2bf5822a0d4f490be419f4c4dc12435df1bb04
sei <listen_spy_air_159493@i.softbank.jp>
2026-04-11 17:03:13 +0900

    認証情報確認apiの実装2

--- app/src/main/java/com/example/app/dto/MeDto.java
+++ app/src/main/java/com/example/app/dto/MeDto.java
@@ -1,4 +1,16 @@
 package com.example.app.dto;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class MeDto {
+
+  private Integer id;
+
+  private String email;
+
+  private List<String> roles;
+
 }
--- app/src/main/java/com/example/app/service/impl/AccountServiceImpl.java
+++ app/src/main/java/com/example/app/service/impl/AccountServiceImpl.java
@@ -1,4 +1,20 @@
 package com.example.app.service.impl;
 
-public class AccountServiceImpl {
+import com.example.app.dto.AccountDto;
+import com.example.app.exception.ApiNotFoundException;
+import com.example.app.mapper.AccountMapper;
+import com.example.app.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountServiceImpl implements AccountService {
+
+  private final AccountMapper accountMapper;
+
+  @Override
+  public AccountDto findByEmail(String email) {
+    return accountMapper.findByEmail(email).orElseThrow(() -> new ApiNotFoundException("不正なリクエストです。"));
+  }
 }
