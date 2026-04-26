5556e5c06d3eea7a64acd5c94392c73a6181bb16
sei <listen_spy_air_159493@i.softbank.jp>
2026-04-26 19:58:06 +0900

    FE:商品登録フォーム2

--- frontend/src/components/pages/ProductCreateForm.tsx
+++ frontend/src/components/pages/ProductCreateForm.tsx
@@ -1,12 +1,12 @@
-import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+import { useForm, type SubmitHandler } from "react-hook-form";
 
 type Inputs = {
   name: string;
   description: string;
   price: number;
   stock: number;
-  imageFiles: File[]; // 一旦考える
-  sortOrder: number;
+  imageFiles: FileList; // 一旦考える
 };
 
 export const ProductCreateForm = () => {
@@ -19,5 +19,94 @@ export const ProductCreateForm = () => {
     reValidateMode: "onSubmit",
   });
 
-  return <></>;
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="name">商品名</label>
+        <div>
+          <input
+            id="name"
+            type="text"
+            {...register("name", {
+              required: "商品名は必須です",
+              maxLength: 60,
+            })}
+          />
+          <p>
+            <ErrorMessage errors={errors} name="name" />
+          </p>
+        </div>
+      </div>
+      <div>
+        <label htmlFor="price">価格(税抜き)</label>
+        <div>
+          <input
+            id="price"
+            type="number"
+            {...register("price", {
+              required: "価格は必須です",
+              min: 100,
+              max: 1000000,
+            })}
+          />
+          <p>
+            <ErrorMessage errors={errors} name="price" />
+          </p>
+        </div>
+      </div>
+      <div>
+        <label htmlFor="stock">在庫数</label>
+        <div>
+          <input
+            id="stock"
+            type="number"
+            {...register("stock", {
+              required: "在庫数は必須です",
+              min: 1,
+              max: 100,
+            })}
+          />
+          <p>
+            <ErrorMessage errors={errors} name="stock" />
+          </p>
+        </div>
+      </div>
+      <div>
+        <label htmlFor="description">備考</label>
+        <div>
+          <textarea
+            id="description"
+            {...register("description", {
+              maxLength: 3000,
+            })}
+          />
+          <p>
+            <ErrorMessage errors={errors} name="description" />
+          </p>
+        </div>
+      </div>
+      <div>
+        <label htmlFor="imageFiles">商品画像</label>
+        <div>
+          <input
+            id="imageFiles"
+            type="file"
+            accept=".png"
+            multiple
+            {...register("imageFiles")}
+          />
+          <p>
+            <ErrorMessage errors={errors} name="imageFiles" />
+          </p>
+        </div>
+      </div>
+      <div>
+        <button type="submit">登録</button>
+      </div>
+    </form>
+  );
 };
--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router";
 import { Login } from "../components/pages/Login";
+import { ProductCreateForm } from "../components/pages/ProductCreateForm";
 import { ProductDetail } from "../components/pages/ProductDetail";
 import { ProductManegement } from "../components/pages/ProductManegement";
 import { PrivateRoute } from "../components/router/PrivateRoute";
@@ -9,6 +10,7 @@ export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/test" element={<ProductCreateForm />} />
       <Route element={<PrivateRoute />}>
         <Route path="/admin/product" element={<DefaultLayout />}>
           <Route path="list" element={<ProductManegement />} />
