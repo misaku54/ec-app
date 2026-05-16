ce1f4bae0fcc8769a21bdfea94910a1174544c6b
sei <listen_spy_air_159493@i.softbank.jp>
2026-05-16 21:02:51 +0900

    zustandの練習

---
+++ frontend/src/practice/Zustand/components/InputText.tsx
@@ -0,0 +1,21 @@
+import { useStore } from "../stores/useStore";
+
+function InputText() {
+  const text = useStore((state) => state.text);
+  const setText = useStore((state) => state.setText);
+
+  console.log("InputTextがレンダリングされた");
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <p>入力したテキスト: {text}</p>
+    </div>
+  );
+}
+
+export default InputText;
