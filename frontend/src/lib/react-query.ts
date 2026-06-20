39e5d2fc2ff188f58344d77ff8efa688ea671242
sei <123493228+misaku54@users.noreply.github.com>
2026-06-20 21:39:59 +0900

    react-query導入2

--- frontend/src/lib/react-query.ts
+++ frontend/src/lib/react-query.ts
@@ -1,4 +1,8 @@
-import { type DefaultOptions } from "@tanstack/react-query";
+import {
+  type DefaultOptions,
+  type UseMutationOptions,
+} from "@tanstack/react-query";
+
 export const queryConfig = {
   queries: {
     // false に設定した場合、ウィンドウにフォーカスが当たってもクエリの再取得は行われません。
@@ -9,3 +13,19 @@ export const queryConfig = {
     staleTime: 60 * 1000,
   },
 } satisfies DefaultOptions;
+
+export type ApiFnReturnType<FnType extends (...args: any) => Promise<any>> =
+  Awaited<ReturnType<FnType>>;
+
+export type QueryConfig<T extends (...args: any[]) => any> = Omit<
+  ReturnType<T>,
+  "queryKey" | "queryFn"
+>;
+
+export type MutationConfig<
+  MutationFnType extends (...args: any) => Promise<any>,
+> = UseMutationOptions<
+  ApiFnReturnType<MutationFnType>,
+  Error,
+  Parameters<MutationFnType>[0]
+>;
